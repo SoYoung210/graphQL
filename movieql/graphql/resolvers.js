b72a3c1aab200ad99643c4affb6f64841b1941cb
SoYoung210 <ethdud1@gmail.com>
2018-10-28 19:00:25 +0900

    요약 : resovler 는 질문을 해결하는 것이다. 다른 api에 갈수도 있고, DB에 접근할 수 도 있다.

--- movieql/graphql/resolvers.js
+++ movieql/graphql/resolvers.js
@@ -1,12 +1,18 @@
+import { people, getById } from "../db"
+/*
 const soyoung = {
   name: "Soyoung",
   age: 23,
   gender: "female"
-}
+}*/
 const resolvers = {
   Query: {
     //name: () => "soyoung"
-    person: () => soyoung
+    people: () => people,
+    //same , person : (_,arg.id) => getById(arg.id)
+    person: (_, { id }) => getById(id)
+
+    //person: () => soyoung
   }
 }
 
