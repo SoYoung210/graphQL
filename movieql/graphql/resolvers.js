a7a303d84ecb6237b483db5bb79eeefd78c5f8a2
SoYoung210 <ethdud1@gmail.com>
2018-10-28 18:38:49 +0900

    graphql 기본

--- movieql/graphql/resolvers.js
+++ movieql/graphql/resolvers.js
@@ -1,6 +1,12 @@
+const soyoung = {
+  name: "Soyoung",
+  age: 23,
+  gender: "female"
+}
 const resolvers = {
   Query: {
-    name: () => "soyoung"
+    //name: () => "soyoung"
+    person: () => soyoung
   }
 }
 
